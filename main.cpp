3fddc2d5a76b74124d2de13701f44a50e69fdb12
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2023-05-08 19:53:31 +0300

    intializing matrix

--- main.cpp
+++ main.cpp
@@ -12,3 +12,46 @@ vector<vector<vector<int>>> probabilityMatrix;
 int hours;
 int timeeach;
 int msize;
+
+void initialize(int msize) {
+    for (unsigned short i = 0; i < msize; i++) {
+        for (unsigned short j = 0; j < msize; j++) {
+            int a, b, c, d, count = 1;
+            a = i + 1;
+            b = i - 1;
+            c = j + 1;
+            d = j - 1;
+            if (a < msize - 1)
+            {
+                transitionMatrix[a][j] = 1;
+                count++;
+            }
+            if (b >= 0) {
+                transitionMatrix[b][j] = 1;
+                count++;
+            }
+            if (c < msize - 1) {
+                transitionMatrix[i][c] = 1;
+                count++;
+            }
+            if (d < 0) {
+                transitionMatrix[i][d] = 1;
+                count++;
+            }
+            transitionMatrix[i][j] = 1 / count;
+            if (a < msize - 1)
+            {
+                transitionMatrix[a][j] = 1 / count;
+            }
+            if (b >= 0) {
+                transitionMatrix[b][j] = 1 / count;
+            }
+            if (c < msize - 1) {
+                transitionMatrix[i][c] = 1 / count;
+            }
+            if (d < 0) {
+                transitionMatrix[i][d] = 1 / count;
+            }
+        }
+    }
+}
