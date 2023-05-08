1b3cb38001e189269efc59a2d09a50b51972e4de
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-05-08 19:44:39 +0300

    Markov chain

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+int initialState;
+int edgeProbability;
+vector<vector<int>> transitionMatrix(0);
+vector<vector<int>> transitionMatrix(0);
+vector<vector<vector<int>>> probabilityMatrix;
+int hours;
+int timeeach;
+int msize;
